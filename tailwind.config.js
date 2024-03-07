3abdd34717810ee70cb8ded51a19366b0b7626d6
David gomez <david.gomez04@hotmail.com>
2024-03-07 12:17:05 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "Strong-Cyan": "hsl(171, 66%, 44%)",
+        "Light-Blue": "hsl(233, 100%, 69%)",
+        "Dark-Grayish-Blue": "hsl(210, 10%, 33%)",
+        "Grayish-Blue": "hsl(201, 11%, 66%)",
+      },
+    },
+  },
+  plugins: [],
+};
